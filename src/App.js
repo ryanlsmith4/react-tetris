dccf79b5c686a0db8fb7e0c867d07a5116ffe03e
Ryan <ryan.l.smith4@gmail.com>
2019-09-23 11:40:43 -0700

    [ADD] added gird square

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import GridSquare from './components/Grid-square';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Tetris Redux</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Tetris Redux</h1>
+        </header>
+          <GridSquare color="1" />
+      </div>
+    )
+  }
 }
 
 export default App;
