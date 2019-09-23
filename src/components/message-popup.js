4a7d5c17f9e9c7dba380530f896d94d331110161
Ryan <ryan.l.smith4@gmail.com>
2019-09-23 14:33:13 -0700

    [ADD] added message popup

---
+++ src/components/message-popup.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MessagePopup extends Component {
+  render() {
+    return (
+      <div className="message-popup">
+        <h1>Message Title</h1>
+        <p>Message info...</p>
+      </div>
+    );
+  };
+};
+
+export default MessagePopup;
