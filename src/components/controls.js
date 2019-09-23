5de6e3109790e180de7c9dea4cf03bb343e5b549
Ryan <ryan.l.smith4@gmail.com>
2019-09-23 15:39:34 -0700

    [ADD] Add connections for controls

--- src/components/controls.js
+++ src/components/controls.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { moveDown, moveLeft, moveRight, rotate } from '../actions';
+
 
 class Controls extends Component {
   render() {
+    const { isRunning } = this.props;
     return (
       <div className="controls">
         {/* left */}
         <button className='control-button' onClick={(e) => {
-
+          if(!isRunning) { return };
+          this.props.moveLeft();
         }}>Left</button>
 
         {/* right */}
         <button className="control-button" onClick={(e) => {
-
+          if(!isRunning) { return };
+          this.props.moveRight();
         }}>Right</button>
 
         {/* rotate */}
         <button className="control-button" onClick={(e) => {
-
+          if(!isRunning) { return };
+          this.props.rotate();
         }}>Rotate</button>
 
         {/* down */}
         <button className="control-button" onClick={(e) => {
-
+          if(!isRunning) { return };
+          this.props.moveDown();
         }}>Down</button>
 
       </div>
@@ -29,4 +37,19 @@ class Controls extends Component {
   };
 };
 
-export default Controls;
+const mapStateToProps = (state) => {
+  return {
+    isRunning: state.game.isRunning
+  };
+};
+
+const mapDispatchToProps = () => {
+  return {
+    moveRight,
+    moveLeft,
+    moveDown,
+    rotate,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps())(Controls);
