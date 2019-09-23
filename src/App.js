9b3e0bd26dafc353de6ee8fd84a621441012b15f
Ryan <ryan.l.smith4@gmail.com>
2019-09-23 14:18:27 -0700

    [ADD] Added styles

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ class App extends Component {
           <h1 className="App-title">Tetris Redux</h1>
         </header>
           <GridBoard color="1" />
-            <NextBlock />
-              <ScoreBoard />
+          <NextBlock />
+          <ScoreBoard />
       </div>
     );
   };
