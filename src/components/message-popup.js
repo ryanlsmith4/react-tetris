00ef4b7b51f615c19420ae9fa295456b8f5fb308
Ryan <ryan.l.smith4@gmail.com>
2019-09-23 15:46:42 -0700

    [ADD] added connection for popup

--- src/components/message-popup.js
+++ src/components/message-popup.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class MessagePopup extends Component {
   render() {
+    const { gameOver, isRunning } = this.props;
+    let message = '';
+    let isHidden = 'hidden';
+    if(gameOver) {
+      message = 'Game Over';
+      isHidden = '';
+    } else if (!isRunning) {
+      message = 'Paused';
+      isHidden = '';
+    } else {
+      message = '???';
+    }
     return (
-      <div className="message-popup">
-        <h1>Message Title</h1>
+      <div className={`message-popup ${isHidden}`}>
+        <h1>{message}</h1>
         <p>Message info...</p>
       </div>
     );
   };
 };
 
-export default MessagePopup;
+const mapStateToProps = (state) => {
+  return {
+    gameOver: state.game.gameOver,
+    isRunning: state.game.isRunning,
+  };
+};
+
+const mapDispatchToProps = () => {
+  return { }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps())(MessagePopup);
